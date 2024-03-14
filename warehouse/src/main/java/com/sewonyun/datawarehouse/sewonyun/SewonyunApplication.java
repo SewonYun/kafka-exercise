e46b7e7d0cf5c165c5ca3efafe77fb051ab346c3
SewonYun <facemen9459@gmail.com>
2024-03-14 20:22:28 +0900

    init: Kafka exercise by spring Java language

---
+++ warehouse/src/main/java/com/sewonyun/datawarehouse/sewonyun/SewonyunApplication.java
@@ -0,0 +1,13 @@
+package com.sewonyun.datawarehouse.sewonyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewonyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewonyunApplication.class, args);
+	}
+
+}
